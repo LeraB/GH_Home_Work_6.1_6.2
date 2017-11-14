e76b3094a03dd2af1c1eda1af141c31ab50e4644
Валерійка <kudravka13@gmail.com>
2017-11-14 20:58:16 +0200

    add count visited

--- home_6.js
+++ home_6.js
@@ -2,6 +2,7 @@ function client(name, second_name, status){
     var userName = name || 'Anonym' ;
     var userStatus = status || 'usual';
     var SecondName = second_name || 'Anonym' ;
+    var count = 0;
     this.Name = function () {
         console.log(userName)
     }
@@ -15,8 +16,9 @@ function client(name, second_name, status){
     }
 
   return  function (){
-        console.log('user data : ' + '\n'+ 'Second name : '+ SecondName  + '\n'+ 'Name : ' + userName  + '\n'+ 'Status : ' + userStatus )
+        console.log('user data : ' + '\n'+ 'Second name : '+ SecondName  + '\n'+ 'Name : ' + userName  + '\n'+ 'Status : ' + userStatus + '\n'+ 'Visits : ' + ++count )
     }
+
 }
 
 var client1= new client('Lera', 'Briantseva', 'usual')
