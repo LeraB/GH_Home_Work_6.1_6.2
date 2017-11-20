e1598b5407b7f356f8f0fb05dace816464253ac7
Валерійка <kudravka13@gmail.com>
2017-11-20 22:51:15 +0200

    added ;

--- home_work_6_1.js
+++ home_work_6_1.js
@@ -1,41 +1,41 @@
 function Client(name, second_name, status) {
-    var userName = name || 'Anonym'
-    var userStatus = status || 'usual'
-    var secondName = second_name || 'Anonym'
+    var userName = name || 'Anonym';
+    var userStatus = status || 'usual';
+    var secondName = second_name || 'Anonym';
     var count = 0;
     var  all_data = '';
 
     this.name = function () {
         return console.log(userName)
-    }
+    };
 
 
     this.new_name = function (name) {
         userName = 'Name : ' + name;
-    }
+    };
 
     this.second_name = function () {
         return console.log(secondName)
-    }
+    };
    this.new_second_name = function () {
         secondName = 'Second name : ' + second_name;
-    }
+    };
 
     this.count = function () {
         return console.log('Visits : ' + count++);
-    }
+    };
     this.status = function () {
         return console.log(userStatus);
-    }
+    };
 
     this.new_status = function () {
         userStatus = 'Status : ' + status;
-    }
+    };
 
     this.f_all_data = function () {
-        all_data = 'user data : \nSecond name : ' + secondName + '\n' + 'Name : ' + userName + '\n' + 'Status : ' + userStatus + '\n' + 'Visits : ' + ++count;
+        all_data = 'user data : \nSecond name : ' + secondName + '\nName : ' + userName + '\nStatus : ' + userStatus + '\nVisits : ' + ++count;
         console.log(all_data)
-    }
+    };
 
     return this
 }
@@ -51,5 +51,4 @@ client2.name();
 client2.status();
 client1.f_all_data();
 client2.f_all_data();
-client2.count()
 client2.count()
